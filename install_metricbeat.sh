28370c568661715ad5f8ea837b7215b8e19807d4
Aleks Luna <54183392+darkneszalex@users.noreply.github.com>
2022-02-22 23:54:41 -0600

    Update install_metricbeat.sh
    added metricbeat template from jai repository

--- install_metricbeat.sh
+++ install_metricbeat.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt install metricbeat -y
-sudo systemctl start metricbeat
-sudo systemctl enable metricbeat
-sudo metricbeat modules enable system
+sudo metricbeat modules enable nginx
+wget https://raw.githubusercontent.com/Jaibw/elk/main/metricbeat-template.yml
+sed 's/USERNAME/luis/g' metricbeat-template.yml > metricbeat.yml
+sudo cp /etc/metricbeat/metricbeat.yml /etc/metricbeat/metricbeat.yml.bak
+sudo cp metricbeat.yml /etc/metricbeat/metricbeat.yml
+
+#https://raw.githubusercontent.com/darkneszalex/elk_training_2022/main/install_metricbeat.sh
