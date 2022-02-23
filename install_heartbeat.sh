1db3386c30a3b7923a1bdb004c2855f1d0c499d6
Aleks Luna <54183392+darkneszalex@users.noreply.github.com>
2022-02-23 02:42:13 -0600

    Update install_heartbeat.sh
    added java and repository for heartbeat EOF

--- install_heartbeat.sh
+++ install_heartbeat.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-curl -L -O https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-8.0.0-x86_64.rpm
-sudo rpm -vi heartbeat-8.0.0-x86_64.rpm
+#curl -L -O https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-8.0.0-x86_64.rpm
+#sudo rpm -vi heartbeat-8.0.0-x86_64.rpm
+yum -y install java-11-openjdk java-11-openjdk-devel -y
+cat > /etc/yum.repos.d/elasticsearch.repo <<EOF
+[elasticsearch-7.x]
+name=Elasticsearch repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
 sudo yum install heartbeat-elastic -y
 wget https://raw.githubusercontent.com/Jaibw/elk/main/heartbeat-template.yml
 wget https://raw.githubusercontent.com/Jaibw/elk/main/icmp.yml
