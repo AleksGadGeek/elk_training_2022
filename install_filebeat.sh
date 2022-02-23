28ea604f006635230666991b958d4da75ac27205
Aleks Luna <54183392+darkneszalex@users.noreply.github.com>
2022-02-22 23:29:47 -0600

    Create install_filebeat.sh
    install filebeat on ubuntu

---
+++ install_filebeat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt install apt-transport-https -y
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee –a /etc/apt/sources.list.d/
+elastic-7.x.list
+sudo apt update -y
+sudo apt install filebeat -y
+sudo systemctl start filebeat
+sudo systemctl enable filebeat
